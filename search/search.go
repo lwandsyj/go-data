24eb8fabe5a822567c33e2cb9818a11eb7b7d887
li <lwaisyj@163.com>
2021-03-01 13:34:20 +0800

    二分查找

--- search/search.go
+++ search/search.go
@@ -15,3 +15,31 @@ func SimpleSearch(arr []interface{}, data interface{}) (index int) {
 	}
 	return
 }
+
+//BinarySearch 二分查找--序列必须是有序的
+func BinarySearch(sortArr []int, data int) int {
+	left := 0
+	count := len(sortArr)
+	if count == 0 {
+		return -1
+	}
+	right := count - 1
+	//循环条件必须保证左边始终小于右边，不然终止循环
+	for left < right {
+		// 查找中间值，go 整数相除放回整数，比如5/2=2
+		mid := (left + right) / 2
+		//查看中间元素是否是要查找的元素
+		if sortArr[mid] == data {
+			return mid
+		} else if sortArr[mid] > data {
+			// 这里必须要求序列是有序，不然这里无法成立
+			// 中间数大于要查找的数据，那么表示如果存在则必须在前半部分
+			right = mid - 1
+		} else {
+			// 否则则后半部分
+			left = mid + 1
+		}
+	}
+	// 查找不到返回-1
+	return -1
+}
